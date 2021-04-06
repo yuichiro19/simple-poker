8b9ba9b192f46fdee379bc6c7e8261de53ff68c8
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-04-07 04:01:59 +0900

    gameモデルバリデーションの設定

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,14 @@
 class Game < ApplicationRecord
+    with_options presence: true do
+        validates :players, numericality: { greater_than_or_equal_to: 2, less_than_or_equal_to: 9 }
+        validates :player1
+        validates :player2
+        validates :player3
+        validates :player4
+        validates :player5
+        validates :player6
+        validates :player7
+        validates :player8
+        validates :player9
+    end
 end
