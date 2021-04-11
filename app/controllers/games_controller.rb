7d9685b8e564f804a405b22f51c6c9e94aa10840
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-04-11 18:39:58 +0900

    create,edit,updateアクションの定義、ルーティング設定

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,8 +5,26 @@ class GamesController < ApplicationController
 
   def new
   end
-
-  def create
-  end
   
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to edit_game_path(@game.id)
+    else
+      render :index
+    end
+  end
+
+  def edit
+    @game = Game.find(params[:id])
+  end
+
+  def update
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:players, :player1, :player2, :player3, :player4, :player5, :player6, :player7, :player8, :player9, :player10)
+  end
 end
